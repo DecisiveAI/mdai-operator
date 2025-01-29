7b292e8fc63caedff07a44872e3b8331fbb92549
o11ies <vika@mydecisive.ai>
2025-01-29 16:27:22 -0500

    subscribe to valkey, fixes

--- api/v1/mdaihub_types.go
+++ api/v1/mdaihub_types.go
@@ -158,11 +158,6 @@ type Observer struct {
 }
 
 type Config struct {
-	// Interval at which to reconcile the Cluster Configuration, applied only if built-in ValKey is enabled and variables defined for Valkey storage.
-	// +kubebuilder:validation:Optional
-	// +kubebuilder:default:="2m"
-	// +kubebuilder:validation:Format:=duration
-	ReconcileLoopInterval *metav1.Duration `json:"reconcileLoopInterval,omitempty" yaml:"reconcileLoopInterval,omitempty"`
 	// EvaluationInterval Specify the interval at which all evaluations are assessed in the Prometheus infrastructure.
 	// Evaluations with explicit `Interval`s will override this value
 	// +kubebuilder:validation:Optional
