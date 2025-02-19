3fd7343a52d1bf78bbbb93a6c5ac06e7cfd9dc0c
Robin Law <Robin-Law@users.noreply.github.com>
2025-02-19 17:09:34 -0600

    use duration add for minid calculation

--- internal/controller/hub_adapter.go
+++ internal/controller/hub_adapter.go
@@ -455,7 +455,7 @@ func (c HubAdapter) ensureVariableSynced(ctx context.Context) (OperationResult, 
 				return OperationResult{}, err
 			}
 			valkeyClient := *c.valKeyClient
-			thresholdID := strconv.FormatInt(time.Now().UnixMilli()-valkeyAuditStreamExpiry.Milliseconds(), 10)
+			thresholdID := strconv.FormatInt(time.Now().Add(-valkeyAuditStreamExpiry).UnixMilli(), 10)
 			if result := valkeyClient.Do(ctx, valkeyClient.B().Xadd().Key(mdaiHubEventHistoryStreamName).Minid().Threshold(thresholdID).Id("*").FieldValue().FieldValueIter(composeValkeyStreamIterFromMap(mdaiHubEvent)).Build()); result.Error() != nil {
 				c.logger.Error(err, "Failed to write audit log entry!", "mdaiHubEvent", mdaiHubEvent)
 			}
