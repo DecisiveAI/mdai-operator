ea19f66e9a7734dd3ecf0f752cdd6629c271b183
Joe Churchman <joe@mydecisive.ai>
2025-04-08 09:42:57 -0700

    chore: Restore check for var configs

--- internal/webhook/v1/mdaihub_webhook.go
+++ internal/webhook/v1/mdaihub_webhook.go
@@ -125,7 +125,7 @@ func (v *MdaiHubCustomValidator) Validate(mdaihub *mdaiv1.MdaiHub) (admission.Wa
 	storageKeys := map[string]struct{}{}
 	exportedVariableNames := map[string]struct{}{}
 	variables := mdaihub.Spec.Variables
-	if variables == nil {
+	if variables == nil || len(*variables) == 0 {
 		warnings = append(warnings, "Variables are not specified")
 	} else {
 		for _, variable := range *variables {
