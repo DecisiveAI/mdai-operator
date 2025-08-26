2b3d8ec9b5f1c38835e582835eae70e1a31e9da8
dmitry <dmitry@mydecisive.ai>
2025-08-26 17:35:16 -0400

    bug fix: resources using generated certificates should be in one file to avoid multiple helper function calls

--- deployment/files/no_cert_manager_option.sh
+++ deployment/files/no_cert_manager_option.sh
@@ -4,6 +4,8 @@ HELM_CHART_ROOT='deployment'
 FILES_DIR="${HELM_CHART_ROOT}/files"
 TEMPLATES_DIR="${HELM_CHART_ROOT}/templates"
 
+YAML_BOUNDARY='---'
+
 WEBHOOK_TEMPLATE_NAME="^.*-webhook-configuration\.yaml$"
 ISSUER_TEMPLATE_NAME="^.*-issuer\.yaml$"
 CERT_TEMPLATE_NAME="^.*-certs?\.yaml$"
@@ -23,8 +25,6 @@ CRT_MGR_OFF_HEADER=$(cat <<'EOF'
 EOF
 )
 
-#  copy secrets template to templates
-cp ${FILES_DIR}/cert_secret.yaml ${TEMPLATES_DIR}/cert-secret.yaml
 
 #  add volumes.yaml addition
 cat ${FILES_DIR}/no_cm_values.yaml >> ${HELM_CHART_ROOT}/values.yaml
@@ -42,18 +42,19 @@ mv ${TEMPLATES_DIR}/.tmp_file ${TEMPLATES_DIR}/_helpers.tpl
 #  add _helpers.tpl block
 cat ${FILES_DIR}/no_cm_helpers.tpl >> ${TEMPLATES_DIR}/_helpers.tpl
 
+# truncate no-cm-secrets-webhooks.yaml
+cp /dev/null ${TEMPLATES_DIR}/no-cm-secrets-webhooks.yaml
+
+# start collecting secrets and webhooks, that use generated certificates: certs
+cat  ${FILES_DIR}/cert_secret.yaml > ${TEMPLATES_DIR}/.tmp_file_1
 
 # - creates copies for webhook templates with no-cert-manager conditional
 for file in ${TEMPLATES_DIR}/*; do
   base=$(basename "$file")
   name="${base%.*}"
-  if [[ "$base" =~ $WEBHOOK_TEMPLATE_NAME ]]; then
-    {
-      printf "%s\n" "$CRT_MGR_OFF_HEADER"
-      cat "$file"
-      printf "\n%s" "$CRT_MGR_FOOTER"
-    } > ${TEMPLATES_DIR}/.tmp_file
 
+  if [[ "$base" =~ $WEBHOOK_TEMPLATE_NAME ]]; then
+    printf "\n%s\n" "$YAML_BOUNDARY" >> "${TEMPLATES_DIR}/.tmp_file_2"
     awk -v key="$CERT_MGR_ANNOTATION" '
     # Skip lines matching the annotation
     index($0, key ":") { next }
@@ -69,9 +70,19 @@ for file in ${TEMPLATES_DIR}/*; do
 
     # Default case: print the line
     { print }
-    ' ${TEMPLATES_DIR}/.tmp_file > "${TEMPLATES_DIR}/${name}-no-cm.yaml"
+    ' ${file} >> "${TEMPLATES_DIR}/.tmp_file_2"
   fi
 done
+
+{
+  printf "%s\n" "$CRT_MGR_OFF_HEADER"
+  cat "${TEMPLATES_DIR}/.tmp_file_1"
+  cat "${TEMPLATES_DIR}/.tmp_file_2"
+  printf "%s" "$CRT_MGR_FOOTER"
+} >> ${TEMPLATES_DIR}/no-cm-secrets-webhooks.yaml
+
+rm ${TEMPLATES_DIR}/.tmp_file_1 ${TEMPLATES_DIR}/.tmp_file_2
+
 
 # - add cert-manager conditional header/footer to webhook, issuer, certs templates
 for file in ${TEMPLATES_DIR}/*; do
