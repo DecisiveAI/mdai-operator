ef6f8ab2db919ab43d91c0d354fabb3a40c562a5
dmitry <dmitry@mydecisive.ai>
2025-10-06 09:23:57 -0400

    fix for Service selector label value

--- internal/controller/common_test.go
+++ internal/controller/common_test.go
@@ -27,18 +27,18 @@ var (
 	selectorLabels = map[string]string{
 		"app.kubernetes.io/component":  "opentelemetry-collector",
 		"app.kubernetes.io/instance":   "test.test",
-		"app.kubernetes.io/managed-by": "mdai-operator",
+		"app.kubernetes.io/managed-by": "opentelemetry-operator",
 	}
 	grpcServiceLabels = map[string]string{
 		"app.kubernetes.io/component":  "opentelemetry-collector",
-		"app.kubernetes.io/managed-by": "mdai-operator",
+		"app.kubernetes.io/managed-by": "opentelemetry-operator",
 		"app.kubernetes.io/instance":   "test.test",
 		"app.kubernetes.io/version":    "latest",
 		"app.kubernetes.io/name":       "test-collector-grpc",
 	}
 	nonGrpcServiceLabels = map[string]string{
 		"app.kubernetes.io/component":  "opentelemetry-collector",
-		"app.kubernetes.io/managed-by": "mdai-operator",
+		"app.kubernetes.io/managed-by": "opentelemetry-operator",
 		"app.kubernetes.io/instance":   "test.test",
 		"app.kubernetes.io/version":    "latest",
 		"app.kubernetes.io/name":       "test-collector-non-grpc",
